a561323bf6ced92cd0b4a494f9766d32341165b4
De1t4 <mauriciochambicaceres@gmail.com>
2024-04-07 17:39:37 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,100 @@
+from fastapi import FastAPI, UploadFile, HTTPException,File
+from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
+import cloudinary
+import cloudinary.uploader 
+from cloudinary import api
+import os
+from dotenv import load_dotenv
+from peewee import MySQLDatabase
+from peewee import Model, CharField, PrimaryKeyField
+
+app = FastAPI()
+load_dotenv()
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+cloudinary.config( 
+    cloud_name=os.getenv("CLOUDINARY_CLOUD_NAME"),
+    api_key=os.getenv("CLOUDINARY_API_KEY"),
+    api_secret=os.getenv("CLOUDINARY_API_SECRET")
+)
+
+
+db = MySQLDatabase(
+  os.getenv("DB_DATABASE"),
+  user=os.getenv("DB_USER"), 
+  password=os.getenv("DB_PASSWORD"), 
+  host=os.getenv("DB_HOST")
+)
+
+class BaseModel(Model):
+  class Meta:
+    database = db
+
+class product(BaseModel):
+  idProduct = PrimaryKeyField()
+  ProductURL = CharField(max_length=255)
+
+# db.connect()
+# fetchData = product.select()
+# for data in fetchData:
+# 	print(data.idProduct, data.ProductURL)
+# db.close()
+
+
+# Ejecutar consulta para crear una tabla
+
+
+@app.post('/upload')
+async def upload_file(file: UploadFile = File(...)):
+	if not file:
+		raise HTTPException(status_code=404, detail="File not found")
+	try:
+		db.connect()
+		response = cloudinary.uploader.upload(file.file, folder="uploads")
+
+		responseURL = response['secure_url']
+		
+		newURL = product(ProductURL=responseURL)
+		newURL.save()
+
+		return JSONResponse(content={"detail": "File received successfully", "url":response['secure_url']}, status_code=200)
+	except Exception as error:
+		print(HTTPException(status_code=500, detail=f"error en el servidor {error}"))
+	finally:
+		db.close()
+
+
+@app.get('/images')
+async def getImage():
+	try:
+
+		# response = api.resources(type ='upload', prefix='uploads/')
+		# return JSONResponse(content={"detail":"images retrieved", "images": response['resources']}, status_code=200)
+		db.connect()
+		fetchData = product.select()
+		dataURL = []
+		for data in fetchData:
+			objectURL = {
+				"idProduct": data.idProduct,
+				"URL":data.ProductURL
+			}
+			dataURL.append(objectURL)
+
+		return dataURL
+
+	except Exception as error:
+			return JSONResponse({"error": error})
+	finally:
+		db.close()
+	
+
+
